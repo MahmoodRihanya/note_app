baa4243cfd141e9835c1b96e0cf450e93b34f4df
mahmood rihanya <https://github.com/MahmoodRihanya>
2025-07-24 16:31:46 +0300

    create note item

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
@@ -6,6 +7,7 @@ class CustomNoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: EdgeInsets.only(top: 24, bottom: 24, left: 18),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(24),
         color: Color(0xffFFCC80),
@@ -14,25 +16,36 @@ class CustomNoteItem extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           ListTile(
-            trailing: Icon(Icons.delete, color: Colors.black),
-            title: Text('Flutter tips', style: TextStyle(color: Colors.black)),
-            subtitle: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Build your career ',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
+            trailing: Icon(FontAwesomeIcons.trash, color: Colors.black),
+            title: Text(
+              'Flutter tips',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16),
+              child: Text(
+                'Build your career with tharwat samy',
+                style: TextStyle(
+                  color: Colors.black.withAlpha(150),
+                  fontSize: 22,
                 ),
-                Text(
-                  'with tharwat samy ',
-                  style: TextStyle(color: Colors.black, fontSize: 16),
-                ),
-              ],
+              ),
             ),
           ),
-          Text(
-            'update at 22/5/2009',
-            style: TextStyle(color: Colors.black, fontSize: 16),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 24),
+            child: Text(
+              '22/5/2009',
+              style: TextStyle(
+                color: Colors.black.withAlpha(150),
+                fontSize: 18,
+              ),
+            ),
           ),
         ],
       ),
